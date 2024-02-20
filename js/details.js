a9f224e4a24a24e7e73f27e8468daf627e84f72c
dm-mensavi <mensavidavid@gmail.com>
2024-02-20 12:18:31 +0000

    Style home page

---
+++ js/details.js
@@ -0,0 +1,23 @@
+// article-details.js
+
+document.addEventListener("DOMContentLoaded", function () {
+	const articleDetails = document.getElementById("articleDetails");
+
+	// Retrieve URL parameters
+	const params = new URLSearchParams(window.location.search);
+	const title = params.get("title");
+	const content = params.get("content");
+
+	// Display the details of the article
+	if (title && content) {
+		const articleElement = document.createElement("article");
+		articleElement.innerHTML = `<h2>${decodeURIComponent(
+			title
+		)}</h2><p>${decodeURIComponent(
+			content
+		)}</p><br></h2><p>${decodeURIComponent(
+			content
+		)}</p><br></h2><p>${decodeURIComponent(content)}</p>`;
+		articleDetails.appendChild(articleElement);
+	}
+});
